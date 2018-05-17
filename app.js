4fab27cce9ca2f7db4d6ed47b36761b3b2ad9ba4
Coelacanth7 <galford151@gmail.com>
2018-05-17 10:35:46 -0700

    refactor html function

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ let state = initialState();
 
 const startBtn = document.getElementById("startBtn");
 const quiz = document.getElementById("quiz");
+const listOfAnswers = document.getElementsByClassName("answers");
 
 startBtn.addEventListener("click", () => {
 	quiz.innerHTML = "";
@@ -28,46 +29,61 @@ startBtn.addEventListener("click", () => {
 });
 
 var getQuestion = () => {
-	questions.length ? createQuestions(questions.shift()) : showScore();
+	questions.length ? createQuestionHtml(questions.shift()) : showScore();
 };
 
-var createQuestions = currentQuestion => {
-	let li, selected;
+var createQuestionHtml = currentQuestion => {
+	let li;
 
 	const questionDiv = document.createElement("div");
+
 	const question = document.createElement("h2");
 	question.innerHTML = currentQuestion.question;
+
+	const btn = document.createElement("button");
+	btn.innerHTML = "submit";
 
 	const ul = document.createElement("ul");
 	currentQuestion.answers.forEach(answer => {
 		li = document.createElement("li");
 		li.innerHTML = answer;
 		li.className = "answers";
-
 		ul.appendChild(li);
 	});
-
-	const btn = document.createElement("button");
-	btn.innerHTML = "submit";
 
 	questionDiv.appendChild(question);
 	questionDiv.appendChild(ul);
 	quiz.appendChild(questionDiv);
 
-	const listOfAnswers = [...document.getElementsByClassName("answers")];
-	console.log(listOfAnswers);
-	[].forEach.call(listOfAnswers, li => {
-		console.log(li);
-		li.style = "color: blue;";
-		li.addEventListener("click", () => {
-			Array.prototype.forEach.call(listOfAnswers, li => {
-				li.classList.remove("selected");
-			});
+	let selectedValue = getSelectedValue();
+};
 
-			selected = li;
-			console.log(selected);
-			li.classList.add("selected");
-		});
+var getSelectedValue = () => {
+	let selected;
+
+	[].forEach.call(listOfAnswers, li => {
+		selected = createListenersOnLi(li);
+	});
+
+	return selected;
+};
+
+var createListenersOnLi = li => {
+	let selected;
+
+	li.addEventListener("click", () => {
+		removeSelectedClassOnLi();
+		selected = li;
+		console.log(selected);
+		li.classList.add("selected");
+	});
+
+	return selected;
+};
+
+var removeSelectedClassOnLi = () => {
+	[].forEach.call(listOfAnswers, li => {
+		li.classList.remove("selected");
 	});
 };
 
